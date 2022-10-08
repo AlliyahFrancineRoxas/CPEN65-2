13f2dc12756ce04f0408df48fab4e3025266f4f3
Alliyah Francine J. Roxas <90834048+AlliyahFrancineRoxas@users.noreply.github.com>
2022-10-08 04:09:23 -1200

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,11 @@
+import math
+#Create  python program that computes the Area of a circle
+
+#Given
+pi=3.141592653589793238
+
+radius = float(input("The radius of a circle:"))
+
+#Formula
+A=pi*radius*radius
+print("The Area of Circle:",A)
